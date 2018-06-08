34a7948d6abe3e4201673c44acb3b6322e292488
vegy <vegycslol@gmail.com>
2018-06-09 00:08:12 +0200

    0.2.0

--- hangman.hs
+++ hangman.hs
@@ -1,39 +1,55 @@
 import Data.List
 
-sentence = "lala haha"
+
+type Sentence = String
+type Hp = Int
+type UsedChars = [Char]
+
+data State = State Sentence Hp UsedChars
+
+data PlayState = Won | Lost | Play
 
 main = do
   putStrLn "Choose starting hp:"
-  hp <- getLine
-  start sentence (read hp ::Integer) []
+  line <- getLine
+  game $ State sentence ((read line)::Hp) [] 
+  where sentence = "lala haha" :: Sentence
 
-start s hp used
-  | hp == 0 = putStrLn ("gg dead, the solution was " ++ s)
-  | (getUnique $ filter (\x -> x /= ' ') s)  \\ used == [] = putStrLn "gg!"
-  | otherwise = do
-    putStrLn output
-    putStrLn "Enter a char:"
-    c <- getLine
-    putStrLn ""
-    case c of
-      [] -> start s hp used
-      [x] -> 
-        if x `elem` used
-          then start s hp used
-        else
-          start s (if x `elem` s then hp else (hp-1)) (x:used)
-      (x:xs) -> if (x:xs) == s then putStrLn "gg nice!" else putStrLn "idiot"
-    where
-      output = "solution so far: " ++ revealed ++ "\nused: " ++ usedRepr ++ ", hp: " ++ (show hp)
-      revealed = getRevealed s used
-      usedRepr = "[" ++ (intercalate ", " $ map (\x -> [x]) used) ++ "]"
+game state@(State sentence hp used) = do
+  case playState of
+    Won -> putStrLn "gg!"
+    Lost -> putStrLn ("lost, the solution was " ++ sentence)
+    Play -> do
+      putStrLn $ getStateRepr state
+      line <- getLine
+      game $ getNewState state line
+  where playState = getPlayState state
 
+getPlayState (State sentence hp used)
+  | hp == 0 = Lost
+  | (getUniqueIgnoreWs sentence) \\ used == [] = Won
+  | otherwise = Play
+
+getStateRepr (State sentence hp used) = stateRepr
+  where
+    stateRepr = revealed ++ "\nused: " ++ usedRepr ++ ", hp: " ++ (show hp)
+    revealed = getRevealed sentence used
+    usedRepr = "[" ++ (intercalate ", " $ map (\x -> [x]) used) ++ "]"
+
+getNewState state@(State sentence hp used) line = case line of
+  [] -> state
+  [x] -> if x `elem` used then state else State sentence (hp-1) (x:used)
+  (x:xs) -> if (x:xs) == sentence
+    then State sentence hp $ getUniqueIgnoreWs sentence
+    else State sentence 0 (x:used)
 
 getRevealed guessingStr usedChars = foldr reveal [] guessingStr
   where
     reveal c acc = (getValC c):acc
     getValC ' ' = ' '
     getValC c = if c `elem` usedChars then c else '_'
+
+getUniqueIgnoreWs = getUnique . filter (\x -> x /= ' ') 
 
 getUnique xs =
   getUnique' xs []
